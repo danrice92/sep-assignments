4ac70d98ab3cdf37f396bde23c1d344a6e1fdf38
Dan Rice <dan.rice.92@outlook.com>
2017-03-14 09:34:55 -0600

    Fixed Screen.rb matrix

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -3,34 +3,40 @@ require_relative 'pixel'
 class Screen
   attr_accessor :width
   attr_accessor :height
+  attr_accessor :matrix
 
   def initialize(width, height)
     @width = width
     @height = height
+    @matrix = { }
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
-    @pixel = pixel
-    @x = validateX(x)
-    @y = validateY(y)
+    x = validateX(x)
+    y = validateY(y)
+    @matrix["#{x}, #{y}"] = pixel
   end
 
   def at(x, y)
-    @pixel
+    @matrix["#{x}, #{y}"]
   end
 
   private
 
   def validateX(x)
     if (x < 0 || x > @width)
-      x = nil
+      nil
+    else
+      x
     end
   end
   
   def validateY(y)
     if (y < 0 || y > @height)
-      y = nil
+      nil
+    else
+      y
     end
   end
 
