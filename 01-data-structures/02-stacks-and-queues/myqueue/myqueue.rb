2d3457faeaffbc6c52c8010b2b148045d66929d4
Dan Rice <dan.rice.92@outlook.com>
2017-03-16 08:34:27 -0600

    Assignment 2: Stacks and Queues complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,27 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue.last
   end
 
   def enqueue(element)
+    @queue.insert(0, element)
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def dequeue
+    dequeueItem = @tail
+    @queue.delete(@tail)
+    @tail = @queue.last
+    dequeueItem
   end
 
   def empty?
+    if @queue.length == 0
+      return true
+    else
+      return false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue_spec.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue_spec.rb
@@ -3,45 +3,45 @@ include RSpec
 require_relative 'myqueue'
 
 RSpec.describe MyQueue, type: Class do
-  let(:q) { MyQueue.new }
+  let(:queue) { MyQueue.new }
 
   describe "#enqueue" do
     it "pushes an item into the head of the queue" do
-      q.enqueue("Rob")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Rob"
-      q.enqueue("Ben")
-      expect(q.head).to eq "Ben"
-      expect(q.tail).to eq "Rob"
+      queue.enqueue("Rob")
+      expect(queue.head).to eq "Rob"
+      expect(queue.tail).to eq "Rob"
+      queue.enqueue("Ben")
+      expect(queue.head).to eq "Ben"
+      expect(queue.tail).to eq "Rob"
     end
   end
 
   describe "#dequeue" do
     it "removes an item from the end of the queue" do
-      q.enqueue("Rob")
-      expect(q.empty?).to eq false
-      q.dequeue
-      expect(q.empty?).to eq true
+      queue.enqueue("Rob")
+      expect(queue.empty?).to eq false
+      queue.dequeue
+      expect(queue.empty?).to eq true
     end
 
     it "updates the head and tail accessors properly" do
-      q.enqueue("Rob")
-      expect(q.head).to eq "Rob"
-      expect(q.tail).to eq "Rob"
-      q.enqueue("Ben")
-      expect(q.head).to eq "Ben"
-      expect(q.tail).to eq "Rob"
+      queue.enqueue("Rob")
+      expect(queue.head).to eq "Rob"
+      expect(queue.tail).to eq "Rob"
+      queue.enqueue("Ben")
+      expect(queue.head).to eq "Ben"
+      expect(queue.tail).to eq "Rob"
     end
   end
 
   describe "#empty?" do
     it "returns true if the queue is emtpy" do
-      expect(q.empty?).to eq true
+      expect(queue.empty?).to eq true
     end
 
     it "returns false if the queue is not empty" do
-      q.enqueue("Rob")
-      expect(q.empty?).to eq false
+      queue.enqueue("Rob")
+      expect(queue.empty?).to eq false
     end
   end
 end
